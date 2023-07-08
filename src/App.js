7bfdf2f421ebdde6b29d5d09f8da7e4f414a0180
Mukund Kumar Verma <21mukund.29@gmail.com>
2023-07-08 23:42:53 +0530

    users is copied

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import Users from './user/pages/Users';
 
 function App() {
-  return <>
+  return <Router >
+    
+    <Switch>
+      <Route path='/' exact>
+        <Users />
+      </Route>
+      <Redirect to ='/' />
+    </Switch>
   
-  </>
+  </Router>
 
 }
 
