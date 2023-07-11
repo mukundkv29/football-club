70cfbe7ce3a6af6fe267dfc1505fb33fecb53ac6
Mukund Kumar Verma <21mukund.29@gmail.com>
2023-07-11 12:33:51 +0530

    PLAYERS done

---
+++ src/data/dummy-players.js
@@ -0,0 +1,63 @@
+export const DUMMY_PLAYERS = [
+    {
+        name: 'ERLING HAALAND',
+        number: '9',
+        position: 'FORWARD',
+        country: 'NORWAY',
+        image: 'https://www.mancity.com/meta/media/e2lawnab/erling-haaland.png?width=376&quality=100'
+    }, {
+        name: 'KEVIN DE BRUYNE',
+        number: '17',
+        position: 'MIDFIELDER',
+        country: 'BELGIUM',
+        image: 'https://www.mancity.com/meta/media/i0fhd2ed/kevin-de-bruyne.png?width=376&quality=100'
+    }, {
+        name: 'RUBEN DIAS',
+        number: '3',
+        position: 'DEFENDER',
+        country: 'Portugal',
+        image: 'https://www.mancity.com/meta/media/vw0b1q45/ruben-dias.png?width=376&quality=100'
+    },
+    {
+        name:'EDERSON',
+        number:31,
+        position:'GOALKEEPER',
+        country:'BRAZIL',
+        image:"https://www.mancity.com/meta/media/0hvgjawq/ederson.png?width=376&quality=100"
+    },
+    {
+        name:'NATHAN AKE',
+        number:6,
+        position:'DEFENDER',
+        country:'NETHERLAND',
+        image:'https://www.mancity.com/meta/media/qkra3cqt/nathan-ake.png?width=376&quality=100'
+    },
+    {
+        name:'RUBEN DIAS',
+        number:7,
+        position:'DEFENDER',
+        country:'PORTUGAL',
+        image:'https://www.mancity.com/meta/media/vw0b1q45/ruben-dias.png?width=376&quality=100'
+    },
+    {
+        name:'JACK GREALISH',
+        number:10,
+        position:'MIDFIELDER',
+        country:'ENGLAND',
+        image:'https://www.mancity.com/meta/media/aoeldoi1/jack-grealish.png?width=376&quality=100'
+    },
+    {
+        name:'JULIAN ALVAREZ',
+        number:19,
+        position:'FORWARD',
+        country:'ARGENTINA',
+        image:'https://www.mancity.com/meta/media/n5upwvdk/julian-alvarez.png?width=376&quality=100'
+    },
+    {
+        name:'PEP GUARDIOLA',
+        number:'M1',
+        position:'MANAGER',
+        country:'SPAIN',
+        image:'https://www.mancity.com/meta/media/1jdgzaax/pep-guardiola.png?width=376&quality=100'
+    },
+]
